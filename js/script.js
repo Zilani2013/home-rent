428c1014244c154c1133c91c775c6948f8d8ce96
zilani <zilanicse13@gmail.com>
2018-02-13 15:42:56 -0700

    running

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+
+$('.navbar-sidebar li').on('click', function(){
+    
+    
+    $('.navbar-sidebar li').removeClass('active');
+    $$('this').addClass('active');
+    
+    
+}); 
+
+
+
+//
+//$(".nav li").on("click", function() {
+//    $(".nav li").removeClass("active");
+//    $(this).addClass("active");
+//  });
+//  
